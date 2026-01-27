7e94a80531c1ae6499373cd57b15587f10d55ff3
ayoubhanine <ayoubhanine2000@gmail.com>
2026-01-27 10:35:26 +0100

    1 ere commit

---
+++ script.js
@@ -0,0 +1,147 @@
+const messageDiv = document.getElementById("message");
+const form = document.getElementById("FormDemande");
+const tableBody = document.getElementById("tableBody");
+const compteur = document.getElementById("compteur");
+const rechercheInput = document.getElementById("recherche");
+const btn=document.getElementById("dark")
+btn.addEventListener("click",()=>{
+    document.body.classList.toggle("darkn")
+})
+let currentPage = 1;
+const itemsPerPage = 5;
+
+let demandes = JSON.parse(localStorage.getItem("demandes2")) || [];
+let demandesAffichees = [...demandes]; // 👈 clé du filtre
+
+function saveDemandes() {
+    localStorage.setItem("demandes2", JSON.stringify(demandes));
+}
+
+function afficherMessage(texte, type) {
+    messageDiv.textContent = texte;
+    messageDiv.className = "message " + type;
+    messageDiv.style.display = "block";
+    setTimeout(() => {
+        messageDiv.style.display = "none";
+    }, 4000);
+}
+
+const cercleCompteur = document.getElementById("cercleCompteur");
+
+function updateCompteur() {
+    cercleCompteur.textContent = demandesAffichees.length;
+}
+const btnRecherche = document.getElementById("btnRecherche");
+
+btnRecherche.addEventListener("click", () => {
+    appliquerFiltre(); // appelle la même fonction que l’input
+});
+
+
+
+function afficherDemandes() {
+    tableBody.innerHTML = "";
+
+    const totalPages = Math.ceil(demandesAffichees.length / itemsPerPage) || 1;
+
+    if (currentPage > totalPages) {
+        currentPage = totalPages;
+    }
+
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+
+    const pageDemandes = demandesAffichees.slice(start, end);
+
+for (let i = 0; i < pageDemandes.length; i++) {
+    const d = pageDemandes[i];
+    const tr = document.createElement("tr");
+    const indexReel = demandes.indexOf(d);
+
+    tr.innerHTML = `
+        <td>${d.nom}</td>
+        <td>${d.prenom}</td>
+        <td>${d.tel}</td>
+        <td>${d.email}</td>
+        <td>${d.motif}</td>
+        <td>${d.date}</td>
+        <td>
+            <button class="btn-delete" onclick="supprimmerDemande(${indexReel})">
+                supprimer
+            </button>
+        </td>
+    `;
+
+    tableBody.appendChild(tr);
+}
+
+
+    updateCompteur();
+    updatePagination(totalPages);
+}
+
+function updatePagination(totalPages) {
+    document.getElementById("pageInfo").textContent =
+        "Page " + currentPage + "/" + totalPages;
+
+    document.getElementById("prevBtn").disabled = currentPage === 1;
+    document.getElementById("nextBtn").disabled = currentPage === totalPages;
+}
+
+function supprimmerDemande(index) {
+    demandes.splice(index, 1);
+    saveDemandes();
+    appliquerFiltre();
+}
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const nom = document.getElementById("nom").value.trim();
+    const prenom = document.getElementById("prenom").value.trim();
+    const tel = document.getElementById("tel").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const motif = document.getElementById("motif").value;
+    const date = document.getElementById("date").value;
+
+    if (nom && prenom && tel && email && motif && date) {
+        demandes.push({ nom, prenom, tel, email, motif, date });
+        saveDemandes();
+        form.reset();
+        afficherMessage("Demande ajoutée avec succès", "success");
+        appliquerFiltre();
+    } else {
+        afficherMessage("Veuillez compléter les champs obligatoires", "error");
+    }
+});
+
+document.getElementById("prevBtn").addEventListener("click", () => {
+    currentPage--;
+    afficherDemandes();
+});
+
+document.getElementById("nextBtn").addEventListener("click", () => {
+    currentPage++;
+    afficherDemandes();
+});
+
+function appliquerFiltre() {
+    const valeur = rechercheInput.value.toLowerCase();
+
+    demandesAffichees = demandes.filter(d =>
+        d.nom.toLowerCase().includes(valeur) ||
+        d.prenom.toLowerCase().includes(valeur) ||
+        d.tel.toString().includes(valeur) ||
+        d.email.toLowerCase().includes(valeur)
+    );
+
+    currentPage = 1;
+    afficherDemandes();
+}
+
+// rechercheInput.addEventListener("input", appliquerFiltre);
+
+document.addEventListener("DOMContentLoaded", () => {
+    demandesAffichees = [...demandes];
+    afficherDemandes();
+});
